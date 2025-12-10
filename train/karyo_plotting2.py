61f80855c95166d0aeb03d5607ae2c64660af91a
neophoca <neophoca@gmail.com>
2025-12-10 08:32:00 +0000

    Plot karyogram predicted & GT

--- train/karyo_plotting2.py
+++ train/karyo_plotting2.py
@@ -3,7 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import torchvision.transforms.functional as TF
 
-def extract_chromosome_crops(img_t, out, score_thresh=0.5, pad=5, out_size=(256, 128)):
+
+def extract_chromosome_crops(img_t, out, score_thresh=0.5, pad=5):
     img = img_t * 0.5 + 0.5
     img = img.clamp(0, 1)
 
@@ -28,28 +29,40 @@ def extract_chromosome_crops(img_t, out, score_thresh=0.5, pad=5, out_size=(256,
             continue
 
         crop = TF.crop(img, top=y1, left=x1, height=y2 - y1, width=x2 - x1)
-        crop = TF.resize(crop, out_size)
         crops.append((crop, int(lab), float(sc)))
 
     return crops
 
 
-def plot_karyogram_from_image(dataset, model, device, idx=0, score_thresh=0.5, pad=5,  out_size=(256, 128), cols=8):
-    model.eval()
-    img_t, _ = dataset[idx]
-    img_t = img_t.to(device)
-
-    with torch.no_grad():
-        out = model([img_t])[0]
-
-    crops = extract_chromosome_crops(img_t.cpu(), out, score_thresh, pad, out_size)
+def plot_karyogram_from_output(img_t, out, idx=0, score_thresh=0.5, pad=5, cols=8):
+    crops = extract_chromosome_crops(img_t.cpu(), out, score_thresh, pad)
     if not crops:
         print("No chromosomes found.")
         return
 
+    # sort by label
     crops.sort(key=lambda x: x[1])
 
-    n = len(crops)
+    # find max H/W
+    hs = [c[0].shape[1] for c in crops]
+    ws = [c[0].shape[2] for c in crops]
+    max_h = max(hs)
+    max_w = max(ws)
+
+    # pad all crops to (max_h, max_w), centered
+    padded = []
+    for crop, lab, sc in crops:
+        _, h, w = crop.shape
+        dh = max_h - h
+        dw = max_w - w
+        top = dh // 2
+        bottom = dh - top
+        left = dw // 2
+        right = dw - left
+        crop_pad = TF.pad(crop, (left, top, right, bottom))  # (L, T, R, B)
+        padded.append((crop_pad, lab, sc))
+
+    n = len(padded)
     rows = math.ceil(n / cols)
 
     fig, axes = plt.subplots(rows, cols, figsize=(cols * 2, rows * 3))
@@ -60,7 +73,7 @@ def plot_karyogram_from_image(dataset, model, device, idx=0, score_thresh=0.5, p
     for ax in axes.ravel():
         ax.axis("off")
 
-    for i, (crop, lab, sc) in enumerate(crops):
+    for i, (crop, lab, sc) in enumerate(padded):
         r = i // cols
         c = i % cols
         ax = axes[r, c]
